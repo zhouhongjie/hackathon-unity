fad8352177bb8e5e95eb206ba364aa0257dd47b9
datpta_d5cntt@epu.edu.vn <datpta_d5cntt@epu.edu.vn@8e68e0b7-6ade-e6dd-701f-e1da4ab1a644>
2014-11-15 11:52:12 +0000

    load game scene

---
+++ trunk/VuforiaGame/Assets/HKT/Scripts/MenuPanelMgr.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuPanelMgr : MonoBehaviour
+{
+
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+		public void PlayGame ()
+		{
+				Application.LoadLevelAsync ("MainScene");
+		}
+}
